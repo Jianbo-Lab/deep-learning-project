b9568e125ff825d06bfb9c94b425418bac7e799e
Jianbo-Lab <jianbochen@berkeley.edu>
2016-10-11 23:03:09 -0700

    add directory core for vae as a class and directory network for encoders and decoders

---
+++ core/vae.py
@@ -0,0 +1,149 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+
+class Variational_Autoencoder():
+    def __init__(self, sess, build_encoder, build_decoder,
+    	batch_size = 100,z_dim = 20,img_dim = 784,dataset = 'mnist',
+    	learning_rate = 0.001,num_epochs = 5):
+        """
+        Inputs:
+        sess: TensorFlow session.
+        build_encoder: A function that layes down the computational 
+        graph for the encoder.
+        build_decoder: A function that layes down the computational 
+        graph for the decoder.
+        batch_size: The number of samples in each batch.
+        z_dim: the dimension of z.
+        img_dim: the dimension of an image. 
+        (Currently, we only consider 28*28 = 784.)
+        dataset: The filename of the dataset. 
+        (Currently we only consider mnist.)
+        learning_rate: The learning rate of the Adam optimizer. 
+        num_epochs: The number of epochs.
+        
+        """
+        self.sess = sess
+        self.build_encoder = build_encoder
+        self.build_decoder = build_decoder
+        self.z_dim = z_dim
+        self.img_dim = img_dim
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.dataset = dataset
+        self.num_epochs = num_epochs
+        
+        if dataset == 'mnist':
+            # Load MNIST data in a format suited for tensorflow. 
+            self.mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+            self.n_samples = self.mnist.train.num_examples
+        
+    def train(self):
+        """ Train VAE for a number of steps."""
+        global_step = tf.Variable(0, trainable = False)
+        # Get images from the mnist dataset.
+        batch_images = self.input()
+
+        # Compute the objective function.
+        loss = self.build_vae()
+         
+        
+        # Build a graph that trains the model with one batch of examples 
+        # and updates the model parameters.
+        optimum = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
+
+        # Build an initialization operation to run.
+        init = tf.initialize_all_variables()
+        self.sess.run(init)
+ 
+        num_batchs = int(self.n_samples / self.batch_size)
+
+        for epoch in xrange(self.num_epochs):
+
+            avg_loss_value = 0.
+
+            for b in xrange(num_batchs):
+
+                # Sample a batch of eps from standard normal distribution.
+                batch_eps = np.random.randn(self.batch_size,self.z_dim)
+
+                # Run a step of adam optimization and loss computation.
+                start_time = time.time()
+                _, loss_value = self.sess.run([optimum,loss],
+                                        feed_dict = {self.images: batch_images,
+                                                    self.batch_eps: batch_eps})
+                duration = time.time() - start_time
+
+                #assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+                avg_loss_value += loss_value / self.n_samples * self.batch_size
+
+            # Later we'll add summary and log files to record training procedures.
+            # For now, we will satisfy with naive printing.
+            print 'Epoch {}: The loss is {}.'.format(epoch + 1,
+                                                            avg_loss_value)
+                    
+    def input(self):
+        """
+        This function reads in one batch of data.
+        """
+        if self.dataset == 'mnist':
+            # Extract images and labels (currently useless) from the next batch.
+            batch_images, _ = self.mnist.train.next_batch(self.batch_size)
+            
+            return batch_images
+            
+    def build_vae(self):
+        """
+        This function builds up VAE from encoder and decoder function
+        in the global environment.
+        """
+        # Add a placeholder for one batch of images
+        self.images = tf.placeholder(tf.float32,[self.batch_size,self.img_dim],
+                                    name = 'images')
+         
+        # Create a placeholder for eps.
+        self.batch_eps = tf.placeholder(tf.float32,[self.batch_size,self.z_dim],name = 'eps') 
+        # Construct the mean and the variance of q(z|x).
+        self.encoder_mean,self.encoder_log_sigma2 = \
+        self.build_encoder(self.images,self.z_dim) 
+        # Compute z from eps and z_mean, z_sigma2.
+        self.batch_z = tf.add(self.encoder_mean, \
+                        tf.mul(tf.sqrt(tf.exp(self.encoder_log_sigma2)), self.batch_eps)) 
+        # Construct the mean of the Bernoulli distribution p(x|z).
+        self.decoder_mean = self.build_decoder(self.batch_z,self.img_dim) 
+        # Compute the loss from decoder (empirically).
+        encoder_loss = \
+            -tf.reduce_sum(self.images * tf.log(1e-10 + self.decoder_mean)
+                           + (1 - self.images) * tf.log(1e-10 + 1 - self.decoder_mean),
+                           1)
+        # Compute the loss from encoder (analytically).
+        decoder_loss = -0.5 * tf.reduce_sum(1 + self.encoder_log_sigma2 
+                                           - tf.square(self.encoder_mean) 
+                                           - tf.exp(self.encoder_log_sigma2), 1)
+        
+        # Add up to the cost.
+        self.cost = tf.reduce_mean(encoder_loss + decoder_loss) 
+        
+        return self.cost
+    
+    def generate(self,num = 10):
+        """
+        This function generates images from VAE.
+        Input:
+        num: The number of images we would like to generate from the VAE.
+        """
+        
+        # At the current stage, we require the number of images to
+        # be generated smaller than the batch size for convenience.
+        assert num <= self.batch_size, \
+        "We require the number of images to be generated smaller than the batch size."
+        
+        # Sample z from standard normals.
+        sampled_z = np.random.randn(self.batch_size,self.z_dim)
+
+        return self.sess.run(self.decoder_mean,
+                      feed_dict = {self.batch_z:sampled_z})
+
+
---
+++ networks/ops.py
@@ -0,0 +1,56 @@
+# This script constructs basic operations/layers in neural networks.
+# The functions here are directly adapted/taken from various TensorFlow tutorials
+# and other resources. (We need to add reference when publishing the codes.)
+import numpy as np
+import tensorflow as tf
+
+def linear(input_, output_size, scope=None, 
+           bias_start=0.0, with_w=False):
+    shape = input_.get_shape().as_list()
+
+    with tf.variable_scope(scope,reuse = None):
+        matrix = tf.get_variable("weights", [shape[1], output_size], tf.float32,
+                                 tf.random_normal_initializer(stddev=1 / np.sqrt(shape[1])))
+        bias = tf.get_variable("bias", [output_size],
+            initializer=tf.constant_initializer(bias_start))
+        if with_w:
+            return tf.matmul(input_, matrix) + bias, matrix, bias
+        else:
+            return tf.matmul(input_, matrix) + bias
+
+def conv2d(input_, output_dim, 
+           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+           name="conv2d"):
+    with tf.variable_scope(name):
+        w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
+                            initializer=tf.truncated_normal_initializer(stddev=stddev))
+        conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+
+        biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+        conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+
+        return conv
+
+def deconv2d(input_, output_shape,
+             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+             name="deconv2d", with_w=False):
+    with tf.variable_scope(name):
+        # filter : [height, width, output_channels, in_channels]
+        w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+                            initializer=tf.random_normal_initializer(stddev=stddev))
+        
+        deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+                                strides=[1, d_h, d_w, 1])
+ 
+
+        biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+        deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+
+        if with_w:
+            return deconv, w, biases
+        else:
+            return deconv
+       
+
+def lrelu(x, leak=0.2, name="lrelu"):
+  return tf.maximum(x, leak*x)
