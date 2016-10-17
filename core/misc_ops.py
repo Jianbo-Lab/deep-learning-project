fac13e2d44d2875bc60704d7e558af8025a56d7e
blfang <billyluefang@gmail.com>
2016-10-16 21:57:45 -0700

    Fixed middle column function

--- core/misc_ops.py
+++ core/misc_ops.py
@@ -1,21 +1,18 @@
 
 import numpy as np
 
-def image2center(x):
+def get_middle_column(x):
     """
     x is an image (in a one-dimensional numpy array)
     we take the center column of the image,
     and sample binary values with probability equal
     to the corresponding pixel value
     """
-    shape = x.shape
 
-    if (len(shape) == 1):
-        num_pixels = shape[0]
-    else:
-        num_pixels = shape[1]
+    if (len(x.shape) == 1):
+        x = x.reshape(1, len(x))
+    num_pixels = x.shape[1]
     width = int(np.sqrt(num_pixels))
-    col = x[xrange(width/2, num_pixels+width/2, width)]
+    col = x[:, xrange(width/2, num_pixels+width/2, width)]
     out = np.random.binomial(1,col)
     return(out)
-
