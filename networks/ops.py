b370f2881c15a975caf45ed153d09dce1f928a2f
Jianbo-Lab <jianbochen@berkeley.edu>
2016-10-16 23:45:17 -0700

    add batchnorm layer

--- networks/ops.py
+++ networks/ops.py
@@ -19,7 +19,7 @@ def linear(input_, output_size, scope=None,
             return tf.matmul(input_, matrix) + bias
 
 def conv2d(input_, output_dim, 
-           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+           k_h=3, k_w=3, d_h=1, d_w=1, stddev=0.02,
            name="conv2d"):
     with tf.variable_scope(name):
         w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
@@ -32,7 +32,7 @@ def conv2d(input_, output_dim,
         return conv
 
 def deconv2d(input_, output_shape,
-             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+             k_h=3, k_w=3, d_h=1, d_w=1, stddev=0.02,
              name="deconv2d", with_w=False):
     with tf.variable_scope(name):
         # filter : [height, width, output_channels, in_channels]
@@ -54,3 +54,40 @@ def deconv2d(input_, output_shape,
 
 def lrelu(x, leak=0.2, name="lrelu"):
   return tf.maximum(x, leak*x)
+
+class batch_norm(object):
+    def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
+        with tf.variable_scope(name):
+            self.epsilon = epsilon
+            self.momentum = momentum
+
+            self.ema = tf.train.ExponentialMovingAverage(decay=self.momentum)
+            self.name = name
+
+    def __call__(self, x, train=True):
+        shape = x.get_shape().as_list()
+
+        if train:
+            with tf.variable_scope(self.name) as scope:
+                self.beta = tf.get_variable("beta", [shape[-1]],
+                                    initializer=tf.constant_initializer(0.))
+                self.gamma = tf.get_variable("gamma", [shape[-1]],
+                                    initializer=tf.random_normal_initializer(1., 0.02))
+                
+                try:
+                    batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
+                except:
+                    batch_mean, batch_var = tf.nn.moments(x, [0, 1], name='moments')
+                    
+                ema_apply_op = self.ema.apply([batch_mean, batch_var])
+                self.ema_mean, self.ema_var = self.ema.average(batch_mean), self.ema.average(batch_var)
+
+                with tf.control_dependencies([ema_apply_op]):
+                    mean, var = tf.identity(batch_mean), tf.identity(batch_var)
+        else:
+            mean, var = self.ema_mean, self.ema_var
+
+        normed = tf.nn.batch_norm_with_global_normalization(
+                x, mean, var, self.beta, self.gamma, self.epsilon, scale_after_normalization=True)
+
+        return normed
