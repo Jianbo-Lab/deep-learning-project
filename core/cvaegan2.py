4b9a25478ccefb3e4f0e44125d1f58812f295eb3
Jianbo-Lab <jianbochen@berkeley.edu>
2016-11-30 21:36:17 -0800

    deep cvae with gan with batchnorm with fully conv no pooling with cgan

---
+++ core/cvaegan2.py
@@ -0,0 +1,344 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+import os
+
+"""
+NOTE: the call to build_encoder and build_discriminator in build_vae needs to modified
+depending on whether the encoder/decoder is fully connected or convolutional
+ctrl+F "TOGGLE" to find the line in this file
+"""
+
+class CVAEGAN():
+    def __init__(self, sess, build_encoder, build_decoder, build_discriminator, checkpoint_name = 'vae_checkpoint',
+        batch_size = 100, z_dim = 20, x_dim = 784, dataset = 'mnist', cond_info_dim=10,
+        learning_rate = 0.001, lr_decay=0.95, lr_decay_freq=1000, num_epochs = 5,load = False,load_file = None, alpha = 10., beta = 0.0001, gamma=1., x_width=28,summaries_dir = './', checkpoint_dir = '../notebook/checkpoints/'):
+        """
+        Inputs:
+        sess: TensorFlow session.
+        build_encoder: A function that lays down the computational
+        graph for the encoder.
+        build_decoder: A function that lays down the computational
+        graph for the decoder.
+        checkpoint_name: The name of the checkpoint file to be saved.
+        batch_size: The number of samples in each batch.
+        z_dim: the dimension of z.
+        x_dim: the dimension of an image.
+        (Currently, we only consider 28*28 = 784.)
+        dataset: The filename of the dataset.
+        (Currently we only consider mnist.)
+        learning_rate: The learning rate of the Adam optimizer.
+        num_epochs: The number of epochs.
+
+        """
+        self.sess = sess
+        self.build_encoder = build_encoder
+        self.build_decoder = build_decoder
+        self.build_discriminator = build_discriminator
+        self.checkpoint_name = checkpoint_name
+        self.z_dim = z_dim
+        self.x_dim = x_dim
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.dataset = dataset
+        self.num_epochs = num_epochs
+        self.load = load
+        self.load_file = load_file
+        self.checkpoint_dir = checkpoint_dir
+        self.lr_decay=lr_decay
+        self.lr_decay_freq=lr_decay_freq
+        self.cond_info_dim = cond_info_dim
+        self.gamma = gamma # down-weight GAN loss
+        self.alpha = alpha # weight for the encoder loss.
+        self.beta = beta # weight for the decoder loss.
+        self.x_width=x_width
+        self.summaries_dir = summaries_dir
+        
+        # if dataset == 'mnist':
+        #     # Load MNIST data in a format suited for tensorflow.
+        #     self.mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+        #     self.n_samples = self.mnist.train.num_examples
+        if dataset == 'mnist' and load == False:
+            # Load MNIST data in a format suited for tensorflow.
+            self.mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+            self.n_samples = self.mnist.train.num_examples
+            self.test_size = 100 # self.mnist.test.num_examples
+            self.xtest, self.ytest = self.mnist.test.images[:100], self.mnist.test.labels[:100]
+        else:
+            self.n_samples = dataset.num_examples
+
+
+        global_step = tf.Variable(0, trainable = False)
+
+
+        self.num_epoch = 0
+        self.num_iter = 0
+        self.log = []
+        self.lr = tf.constant(self.learning_rate)
+
+        # Compute the objective function.
+        self.vae_loss, self.dis_loss = self.build_vae()
+
+        vars = tf.trainable_variables()
+        dis_vars = [v for v in vars if v.name.startswith('D/')]
+        vae_vars = [v for v in vars if v.name.startswith('G/')]
+
+
+
+        # Build a graph that trains the model with one batch of examples
+        # and updates the model parameters.
+        self.vae_opt = tf.train.AdamOptimizer(self.lr).minimize(self.vae_loss, var_list=vae_vars)
+        self.dis_opt = tf.train.AdamOptimizer(self.lr).minimize(self.dis_loss, var_list=dis_vars)
+
+        # Build an initialization operation to run.
+        init = tf.initialize_all_variables()
+        self.sess.run(init)
+        if self.load:
+            self.saver = tf.train.Saver()
+            self.saver.restore(self.sess, self.load_file)
+
+        # Add summary writer.
+        self.summary_vae_loss = tf.scalar_summary('loss for vae', self.vae_loss)
+        self.summary_dis_loss = tf.scalar_summary('loss for discriminator', self.dis_loss)
+        self.summary_dec_loss = tf.scalar_summary('loss for decoder', self.decoder_loss)
+        self.summary_enc_loss = tf.scalar_summary('loss for encoder', self.enc_loss)
+        self.summary_fool_loss = tf.scalar_summary('loss for fooling the discriminator', self.xentropy_fresh_mean)
+
+        merged = tf.merge_all_summaries()
+        self.summary_writer = tf.train.SummaryWriter(self.summaries_dir,self.sess.graph)
+
+
+    def train(self):
+        """ Train VAE for a number of steps."""
+        start_time = time.time()
+
+
+        num_batches = int(self.n_samples / self.batch_size)
+
+        for epoch in xrange(self.num_epochs):
+            avg_gen_loss_value = 0.
+            avg_dis_loss_value = 0.
+
+            epoch_start = time.time()
+
+            for b in xrange(num_batches):
+                # Get images from the mnist dataset.
+                batch_images, batch_info = self.input()
+
+                # Sample a batch of eps from standard normal distribution.
+                batch_eps = np.random.randn(self.batch_size,self.z_dim)
+                batch_eps2 = np.random.randn(self.batch_size,self.z_dim)
+
+                # Initialize labels for real and fake data. 
+
+                #label_extra_real = tf.concat(1,[batch_info,tf.tile([[0]],[self.batch_size,1])])
+                label_extra_real = np.hstack((batch_info,np.tile(0,(self.batch_size,1)))).astype(np.int32)
+                label_extra_fresh = np.hstack((np.tile(0,(self.batch_size,self.cond_info_dim)),np.tile(1,(self.batch_size,1)))).astype(np.int32)
+                #label_extra_fresh = tf.concat(1,[tf.tile([[0]],[self.batch_size,self.cond_info_dim]),tf.tile([[1]],[self.batch_size,1])])
+                
+                # Run a step of adam optimization and loss computation. 
+                _, vae_loss, x_mean, x_fresh,encoder_loss,decoder_loss,fake_loss = self.sess.run(
+                    [self.vae_opt, self.vae_loss, self.x_mean, self.x_fresh,
+                    self.enc_loss,self.decoder_loss,self.xentropy_fresh_mean],
+                                            feed_dict = {self.x: batch_images,
+                                                        self.y: batch_info,
+                                                        self.eps: batch_eps,
+                                                        self.z_fresh: batch_eps2,
+                                                        self.lr: self.learning_rate,
+                                                        self.train_phase: True, 
+                                                        self.label_extra_fresh: label_extra_fresh})
+                _, dis_loss = self.sess.run([self.dis_opt, self.dis_loss],
+                                            feed_dict = {self.x: batch_images,
+                                                        #self.eps: batch_eps,
+                                                        self.x_fresh: x_fresh,
+                                                        self.x_mean: x_mean,
+                                                        self.lr: self.learning_rate,
+                                                        self.train_phase: True,
+                                                        self.label_extra_real: label_extra_real,
+                                                        self.label_extra_fresh: label_extra_fresh})
+                loss_value = vae_loss + dis_loss
+
+                duration = time.time() - start_time
+
+                assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+                avg_gen_loss_value += vae_loss / self.n_samples * self.batch_size
+                avg_dis_loss_value += dis_loss / self.n_samples * self.batch_size
+                print 'Epoch {} Generator loss: {} Discriminator loss: {}'.format(self.num_epoch, vae_loss, dis_loss)
+                print 'Epoch {} Encoder loss: {} Decoder loss: {} Fake loss: {}'.format(self.num_epoch, encoder_loss, decoder_loss, fake_loss)
+                self.num_iter += 1
+
+ 
+                if self.num_iter % self.lr_decay_freq == 1:
+                    self.learning_rate *= self.lr_decay
+                if b % 50 == 0:
+
+                                      # Sample a batch of eps from standard normal distribution.
+                    batch_eps = np.random.randn(self.test_size,self.z_dim)
+                    batch_eps2 = np.random.randn(self.test_size,self.z_dim)
+
+                    # Initialize labels for real and fake data. 
+ 
+                    label_extra_real = np.hstack((self.ytest,np.tile(0,(self.test_size,1)))).astype(np.int32)
+                    label_extra_fresh = np.hstack((np.tile(0,(self.test_size,self.cond_info_dim)),np.tile(1,(self.test_size,1)))).astype(np.int32)
+              
+                    # Add training loss_l to self.summary_writer.  
+                    aa,bb,cc,dd,x_mean, x_fresh = self.sess.run([self.summary_vae_loss, self.summary_dec_loss,self.summary_enc_loss,
+                                                             self.summary_fool_loss,self.x_mean, self.x_fresh],
+                                                                feed_dict = {self.x: self.xtest,
+                                                                    self.y: self.ytest,
+                                                                    self.eps: batch_eps,
+                                                                    self.z_fresh: batch_eps2,
+                                                                    self.lr: self.learning_rate,
+                                                                    self.train_phase: True, 
+                                                                    self.label_extra_fresh: label_extra_fresh})  
+                     
+                    summaries2 = self.sess.run(self.summary_dis_loss, 
+                                            feed_dict ={self.x: self.xtest,
+                                                        #self.eps: batch_eps,
+                                                        self.x_fresh: x_fresh,
+                                                        self.x_mean: x_mean,
+                                                        self.lr: self.learning_rate,
+                                                        self.train_phase: True,
+                                                        self.label_extra_real: label_extra_real,
+                                                        self.label_extra_fresh: label_extra_fresh}) 
+
+                    self.summary_writer.add_summary(aa, b + epoch * num_batches)
+                    self.summary_writer.add_summary(bb, b + epoch * num_batches)
+                    self.summary_writer.add_summary(cc, b + epoch * num_batches)
+                    self.summary_writer.add_summary(dd, b + epoch * num_batches)
+                    self.summary_writer.add_summary(summaries2, b + epoch * num_batches)
+            # Later we'll add summary and log files to record training procedures.
+            # For now, we will satisfy with naive printing.
+            self.num_epoch += 1
+            epoch_end = time.time()
+            print 'Epoch {} Generator loss: {} Discriminator loss: {} (time: {} s)'.format(self.num_epoch, avg_gen_loss_value, avg_dis_loss_value, epoch_end-epoch_start)
+            self.save(self.num_epoch)
+        end_time = time.time()
+        print '{} min'.format((end_time-start_time)/60)
+        #self.save(epoch)
+
+    def save(self,epoch):
+        self.saver = tf.train.Saver()
+        self.saver.save(self.sess, os.path.join(self.checkpoint_dir,
+            self.checkpoint_name), global_step = epoch)
+
+    def input(self):
+        """
+        This function reads in one batch of data.
+        """
+        if self.dataset == 'mnist':
+            # Extract images and labels (currently useless) from the next batch.
+            batch_images, batch_info = self.mnist.train.next_batch(self.batch_size)
+
+            return batch_images, batch_info
+
+        batch_images, batch_info = self.dataset.next_batch(self.batch_size)
+        return batch_images, batch_info
+
+    def build_vae(self):
+        """
+        This function builds up VAE from encoder and decoder function
+        in the global environment.
+        """
+        self.train_phase = tf.placeholder(tf.bool, name='train_phase')
+        # Add a placeholder for one batch of images
+        with tf.variable_scope('G'):
+            self.x = tf.placeholder(tf.float32,[self.batch_size,self.x_dim], name = 'x')
+
+            self.y = tf.placeholder(tf.float32,[self.batch_size, self.cond_info_dim], name='y')
+
+            # Create a placeholder for eps.
+            self.eps = tf.placeholder(tf.float32,[self.batch_size, self.z_dim], name = 'eps')
+
+            # Construct the mean and the variance of q(z|x).
+            """
+            === TOGGLE ===
+            NOTE: the call to build_encoder in build_vae needs to modified
+            depending on whether the encoder/decoder is fully connected or convolutional
+            """
+            # use this line for fc:
+            #self.z_mean, self.z_log_sigma2 = self.build_encoder(tf.concat(1, (self.x, self.y)), self.z_dim)
+
+            # use this line for conv:
+            self.z_mean, self.z_log_sigma2 = self.build_encoder(self.x, self.y, self.z_dim, x_width=self.x_width)
+
+            # Compute z from eps and z_mean, z_sigma2.
+            self.z = tf.add(self.z_mean, tf.mul(tf.sqrt(tf.exp(self.z_log_sigma2)), self.eps))
+
+            # Construct the mean of the Bernoulli distribution p(x|z).
+            self.x_mean = self.build_decoder(tf.concat(1, (self.z, self.y)), self.x_dim,train_phase = self.train_phase)
+
+
+            self.z_fresh = tf.placeholder(tf.float32,[self.batch_size, self.z_dim], name = 'z_fresh')
+            self.x_fresh = self.build_decoder(tf.concat(1, (self.z_fresh, self.y)), self.x_dim, reuse=True)
+
+        with tf.variable_scope('D'):
+
+            """
+            === TOGGLE ===
+            NOTE: if using fully connected, comment out the x_width lines
+            """
+
+            # true image
+            self.dis_real,self.lth_layer_real = self.build_discriminator(self.x,
+                x_width=self.x_width,train_phase = True)
+
+            # # reconstruction
+            self.dis_fake_recon,self.lth_layer_recon = self.build_discriminator(self.x_mean, reuse=True,
+                x_width=self.x_width,train_phase = True)
+
+            # fresh generation
+            self.dis_fake_fresh, self.lth_layer_fresh = self.build_discriminator(self.x_fresh, reuse=True,
+                x_width=self.x_width,train_phase = True) 
+
+        # Compute the loss from encoder (analytically).
+        self.encoder_loss = -0.5 * tf.reduce_sum(1 + self.z_log_sigma2
+                                           - tf.square(self.z_mean)
+                                           - tf.exp(self.z_log_sigma2), 1)
+        # Compute the loss from decoder (empirically).
+        self.decoder_loss =  tf.reduce_sum(tf.square(self.lth_layer_real - self.lth_layer_recon)) 
+
+        #cross_entropy_recon = tf.nn.sparse_softmax_cross_entropy_with_logits(self.dis_recon, self.label_extra, name='xentropy_recon') 
+        #dis_loss = tf.reduce_mean(cross_entropy_real + cross_entropy_fresh + cross_entropy_recon, name='xentropy_mean') 
+
+        self.label_extra_real = tf.placeholder(tf.int32,[self.batch_size, self.cond_info_dim + 1], name='label_extra_real')
+        self.label_extra_fresh = tf.placeholder(tf.int32,[self.batch_size, self.cond_info_dim + 1], name='label_extra_fresh')
+
+        self.cross_entropy_real = tf.nn.softmax_cross_entropy_with_logits(self.dis_real, 
+            self.label_extra_real, name='xentropy_real')
+
+        self.cross_entropy_fresh = tf.nn.softmax_cross_entropy_with_logits(self.dis_fake_fresh, 
+            self.label_extra_fresh, name='xentropy_fresh')
+
+
+        dis_loss = tf.reduce_mean(self.cross_entropy_real + self.cross_entropy_fresh, name='xentropy_mean') 
+    
+        # Add up to the cost.
+        self.enc_loss = tf.reduce_mean(self.encoder_loss) 
+        self.xentropy_fresh_mean = tf.reduce_mean(self.cross_entropy_fresh)
+        vae_loss = self.alpha * self.enc_loss + self.beta * self.decoder_loss - self.gamma * self.xentropy_fresh_mean 
+
+        return vae_loss, dis_loss
+
+    def generate(self,num = 10, info = None, load = False):
+        """
+        This function generates images from VAE.
+        Input:
+        num: The number of images we would like to generate from the VAE.
+        """
+
+        # At the current stage, we require the number of images to
+        # be generated smaller than the batch size for convenience.
+        assert num <= self.batch_size, \
+        "We require the number of images to be generated smaller than the batch size."
+        # Sample z from standard normals.
+        sampled_z = np.random.randn(self.batch_size,self.z_dim)
+
+        return self.sess.run(self.x_mean,\
+                      feed_dict = {self.z:sampled_z, self.y:info, self.train_phase: False})
+
+    def get_code(self, img):
+        return self.sess.run(self.z_mean, feed_dict={self.x:img})
+
---
+++ networks/cvaegan_conv3.py
@@ -0,0 +1,192 @@
+import numpy as np
+import tensorflow as tf
+# from ops import *
+slim = tf.contrib.slim
+
+class Encoder:
+    def __init__(self, hidden_dim = 100, x_width=28):
+        self.hidden_dim = hidden_dim
+        self.x_width = x_width
+
+    def __call__(self, x, y, z_dim, batch_size=100,train_phase = True):
+        """
+        The probabilistic encoder which computes the mean and the log
+        of variance of z drawn from the Gaussian distribution q(z|images).
+
+        Inputs:
+        images: A batch of images.
+        z_dim: The dimension of hidden variable z.
+
+        Outputs:
+        A batch of means and log of the variances of z, each corresponding
+        to a single image in images.
+
+        """
+
+        #self.hidden_dims = [self.hidden_dim] * 2
+        net = tf.reshape(x, [batch_size, self.x_width, self.x_width, -1])
+        net = slim.conv2d(net, self.x_width, [5,5], stride=2,
+            normalizer_fn=slim.batch_norm,
+            normalizer_params={'is_training':train_phase,
+                'scale':True,
+                'updates_collections':None,
+                'scope':'enc_bn0'}) 
+        net = slim.conv2d(net, self.x_width * 2, [5,5], stride=2,
+            normalizer_fn=slim.batch_norm,
+            normalizer_params={'is_training':train_phase,
+                'scale':True,
+                'updates_collections':None,
+                'scope':'enc_bn1'}) 
+        net = slim.conv2d(net, self.x_width * 4, [5,5], stride=2,
+            normalizer_fn=slim.batch_norm,
+            normalizer_params={'is_training':train_phase,
+                'scale':True,
+                'updates_collections':None,
+                'scope':'enc_bn2'})
+        net = tf.reshape(net, [batch_size, -1])
+        net = tf.concat(1, (net,y)) 
+        z_mean = slim.fully_connected(net, z_dim, scope='z_mean', activation_fn=None)
+        z_log_sigma_sq = slim.fully_connected(net, z_dim, scope='z_log_sigma', activation_fn=None)
+
+        """
+
+        h0 = tf.nn.softplus(linear(x, self.hidden_dim, scope = 'en_fc0'))
+
+        h1 = tf.nn.softplus(linear(h0, self.hidden_dim, scope = 'en_fc1'))
+
+        z_mean = linear(h1, z_dim, scope = 'z_mean')
+        z_log_sigma_sq = linear(h1, z_dim, scope = 'z_log_sigma')
+        """
+        return (z_mean, z_log_sigma_sq)
+
+class Decoder:
+    def __init__(self, hidden_dim = 100,x_width = 28, layer = 4,x_depth = 1):
+        self.hidden_dim = hidden_dim
+        self.x_width = x_width
+        self.layer = 4
+        self.x_depth = x_depth
+    def __call__(self, z, x_dim, batch_size=100, reuse=None,train_phase = True):
+        """
+        The probabilistic decoder which computes the mean of x drawn from
+        the Bernoulli distribution p(x|z).
+
+        Inputs:
+        z: A batch of hidden variables.
+        x_dim: The dimension of one input image.
+
+        Outputs:
+        x_mean: A batch of the means of p(x|z), each corresponding to a single z.
+        """
+  
+
+        if self.layer == 4:
+            network_size = self.x_width / 4
+
+            net = z
+            net = slim.fully_connected(net, network_size * network_size *128, reuse=reuse, scope='dec_fc0',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'reuse':reuse,
+                    'is_training':train_phase,
+                    'scale':True,
+                    'updates_collections':None,
+                    'scope':'dec_bn0'})
+            net = tf.reshape(net, [batch_size, network_size , network_size , 128])   
+            net = slim.conv2d_transpose(net, 64, [5,5], stride=2, reuse=reuse, scope='dec_conv0',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'reuse':reuse,
+                    'is_training':train_phase,
+                    'scale':True,
+                    'updates_collections':None,
+                    'scope':'dec_bn1'})  
+            net = slim.conv2d_transpose(net, 32, [5,5], stride=2, reuse=reuse, scope='dec_conv1',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'reuse':reuse,
+                    'is_training':train_phase,
+                    'scale':True,
+                    'updates_collections':None,
+                    'scope':'dec_bn2'})  
+            x_mean = slim.conv2d_transpose(net, self.x_depth, [1,1], stride=1, reuse=reuse, scope='x_mean', activation_fn=tf.nn.sigmoid)
+            x_mean = tf.reshape(x_mean,[batch_size,-1])   
+
+            return x_mean
+        elif self.layer == 5:
+            network_size = self.x_width / 8
+            net = z
+            net = slim.fully_connected(net, network_size*network_size*256, reuse=reuse, scope='dec_fc0',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'reuse':reuse,
+                    'is_training':train_phase,
+                    'scale':True,
+                    'updates_collections':None,
+                    'scope':'dec_bn0'})
+            net = tf.reshape(net, [batch_size, network_size, network_size, 256])   
+            net = slim.conv2d_transpose(net, 256, [5,5], stride=2, reuse=reuse, scope='dec_conv0',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'reuse':reuse,
+                    'is_training':train_phase,
+                    'scale':True,
+                    'updates_collections':None,
+                    'scope':'dec_bn1'})  
+            net = slim.conv2d_transpose(net, 128, [5,5], stride=2, reuse=reuse, scope='dec_conv1',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'reuse':reuse,
+                    'is_training':train_phase,
+                    'scale':True,
+                    'updates_collections':None,
+                    'scope':'dec_bn2'}) 
+            net = slim.conv2d_transpose(net, 32, [5,5], stride=2, reuse=reuse, scope='dec_conv2',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'reuse':reuse,
+                    'is_training':train_phase,
+                    'scale':True,
+                    'updates_collections':None,
+                    'scope':'dec_bn3'}) 
+            x_mean = slim.conv2d_transpose(net, 1, [1,1], stride=1, reuse=reuse, scope='x_mean', activation_fn=tf.nn.sigmoid)
+            x_mean = tf.reshape(x_mean,[batch_size,-1])   
+
+            return x_mean           
+
+class Discriminator:
+    def  __init__(self, hidden_dim = 100,x_width = 28):
+        self.hidden_dim = hidden_dim
+        self.x_width = x_width
+    def __call__(self, x, batch_size=100, reuse=None, train_phase = True):
+
+        #self.hidden_dims = [self.hidden_dim] * 4
+        net = tf.reshape(x, [batch_size, self.x_width, self.x_width, -1])
+        net = slim.conv2d(net, self.x_width, [5,5], stride=2, reuse=reuse, scope='dis_conv0',
+            normalizer_fn=slim.batch_norm,
+            normalizer_params={ 'reuse':reuse,
+                                'is_training':train_phase,
+                                'scale':True,
+                                'updates_collections':None,
+                                'scope':'dis_conv_bn0'})
+        #net = slim.max_pool2d(net, [2,2], stride=2)
+        net = slim.conv2d(net, self.x_width * 2, [5,5], stride=2, reuse=reuse, scope='dis_conv1',
+            normalizer_fn=slim.batch_norm,
+            normalizer_params={ 'reuse':reuse,
+                                'is_training':train_phase,
+                                'scale':True,
+                                'updates_collections':None,
+                                'scope':'dis_conv_bn1'})
+        #net = slim.max_pool2d(net, [2,2], stride=2)
+        net = slim.conv2d(net, self.x_width * 4, [5,5], stride=2, reuse=reuse, scope='dis_conv2',
+            normalizer_fn=slim.batch_norm,
+            normalizer_params={ 'reuse':reuse,
+                                'is_training':train_phase,
+                                'scale':True,
+                                'updates_collections':None,
+                                'scope':'dis_conv_bn2'})
+        net = slim.conv2d(net, self.x_width * 8, [5,5], stride=2, reuse=reuse, scope='dis_conv3',
+            normalizer_fn=slim.batch_norm,
+            normalizer_params={ 'reuse':reuse,
+                                'is_training':train_phase,
+                                'scale':True,
+                                'updates_collections':None,
+                                'scope':'dis_conv_bn3'})
+        net = tf.reshape(net, [batch_size, -1]) 
+        lth_layer = slim.fully_connected(net, 500, reuse=reuse, scope='dis_fc0')
+        #for i in xrange(len(self.hidden_dims)):
+            #net = slim.fully_connected(net, self.hidden_dims[i], scope='dis_fc{}'.format(i), activation_fn=tf.tanh, reuse=reuse)
+        logits = slim.fully_connected(lth_layer, 11, scope='logits', activation_fn=tf.nn.sigmoid, reuse=reuse)
+        return logits, lth_layer
